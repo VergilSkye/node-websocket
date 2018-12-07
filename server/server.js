a635ddd495ad9a310f7e0cdcd673822f4dc3d885
cgest Virgílio <E007250@rede.tst>
2018-12-07 16:30:52 -0200

    Exemplo socket.io usuário conectado e desconectado

--- server/server.js
+++ server/server.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const http = require('http');
 const path = require('path');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 
+const socketIO = require('socket.io');
+
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000 ;
-const app = express();
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 //app.set('views', publicPath);
 app.use(logger('dev'));
@@ -14,9 +19,18 @@ app.use(bodyParser.urlencoded());
 
 app.use(express.static(publicPath));
 
+io.on('connection', (socket) => {
+    console.log('New user connected');
 
-app.listen(port, () => {
-    console.log('Express server listening on port ',port);
+    socket.on('disconnect', (socket) => {
+        console.log('User was disconnected');            
+    });
+});
+
+
+
+server.listen(port, () => {
+    console.log('Express server listening on port ',port); 
 });
 
 console.log(publicPath);
