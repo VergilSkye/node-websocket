07da89755e32b8f08e4b2655b92fa4da989166e3
cgest Virgílio <E007250@rede.tst>
2018-12-10 15:52:20 -0200

    Saudando novo usuario e alertando os outros

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const favicon = require('serve-favicon');
 
 const socketIO = require('socket.io');
 
-const port = process.env.PORT || 3000 ;
+const port = process.env.PORT || 3000;
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
@@ -16,28 +16,45 @@ var io = socketIO(server);
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
-app.use(favicon(path.join(__dirname,'../public','images','favicon.ico')));
+app.use(favicon(path.join(__dirname, '../public', 'images', 'favicon.ico')));
 
 app.use(express.static(path.join(__dirname, '../public')));
 
 io.on('connection', (socket) => {
     console.log('New user connected');
 
-    socket.on('createMessage', (message)=>{
-        console.log('createMessagge',message); 
-        
-        io.emit('newMessage',{
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app',
+        createdAt: new Date().getTime()
+    });
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined',
+        createdAt: new Date().getTime()
+    });
+
+    socket.on('createMessage', (message) => {
+        console.log('createMessagge', message);
+
+
+        io.emit('newMessage', {
             from: message.from,
             text: message.text,
             createdAt: new Date().getTime()
         });
-    }); 
-    
+        // socket.broadcast.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createAt: new Date().getTime()
+        // });
+    });
+
     socket.on('disconnect', (socket) => {
-        console.log('User was disconnected');            
+        console.log('User was disconnected');
     });
 });
 
 server.listen(port, () => {
-    console.log('Express server listening on port ',port); 
+    console.log('Express server listening on port ', port);
 });
