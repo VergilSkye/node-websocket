0211e8faf411c681339f093210ea9d7374a16036
cgest Virgílio <E007250@rede.tst>
2018-12-13 15:08:12 -0200

    Adicionando mustache para o template

--- public/js/index.js
+++ public/js/index.js
@@ -10,23 +10,42 @@ socket.on('disconnect', (socket) => {
 });
 
 socket.on('newMessage', (message) => {
-    var formattedTime = moment(message.createdAt).format('h:mm a');
-    console.log('newMessage', message);
-    const li = jQuery('<li></li>');
-    li.text(`${message.from} ${formattedTime}: ${message.text}`);
-    jQuery('#messages').append(li)
+    const formattedTime = moment(message.createdAt).format('h:mm a');
+    const template = jQuery('#message-template').html();
+    const html = Mustache.render(template,{
+        text:message.text,
+        from:message.from,
+        createAt:formattedTime
+    });
+
+    jQuery('#messages').append(html);
+
+        
+    // const li = jQuery('<li></li>');
+    // li.text(`${message.from} ${formattedTime}: ${message.text}`);
+
+    // jQuery('#messages').append(li)
 });
 
 
 socket.on('newLocationMessage', function (message) {
     
-    const li = jQuery('<li></li>');
-    const a = jQuery('<a target="_blank">My current location</a>');
-    var formattedTime = moment(message.createdAt).format('h:mm a');
-    li.text(`${message.from} ${formattedTime}: `);
-    a.attr('href', message.url);
-    li.append(a);
-    jQuery('#messages').append(li)
+    const formattedTime = moment(message.createdAt).format('h:mm a');
+    const template = jQuery('#location-message-template').html();
+    const html = Mustache.render(template,{
+        url: message.url,
+        from: message.from,
+        createAt:formattedTime
+    });
+    jQuery('#messages').append(html);
+
+    // const li = jQuery('<li></li>');
+    // const a = jQuery('<a target="_blank">My current location</a>');
+ 
+    // li.text(`${message.from} ${formattedTime}: `);
+    // a.attr('href', message.url);
+    // li.append(a);
+    // jQuery('#messages').append(li)
 
 });
 
